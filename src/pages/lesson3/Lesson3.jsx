248dd1c2242478563926d6c77cb970696d3c1cc5
liyongjiang <liyongjiang@antiy.cn>
2019-08-23 10:20:39 +0800

    no message

--- src/pages/lesson3/Lesson3.jsx
+++ src/pages/lesson3/Lesson3.jsx
@@ -5,7 +5,8 @@ const users = [
   { username: 'Jerry', age: 21, gender: 'male' },
   { username: 'Tomy', age: 22, gender: 'male' },
   { username: 'Lily', age: 19, gender: 'female' },
-  { username: 'Lucy', age: 20, gender: 'female' }
+  { username: 'Lucy', age: 20, gender: 'female' },
+  { username: 'Lucy1', age: 25, gender: 'female' },
 ]
 class User extends Component {
 
